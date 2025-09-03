c36a8351497ee4621a0c3b3d3e8e70a54d7ee93c
nhathoang1505 <nhathoang1505dmx@gmail.com>
2025-09-03 11:07:07 +0700

    Update WebServer.java

--- WebServer.java
+++ WebServer.java
@@ -13,10 +13,9 @@ public class WebServer {
         server.start();
         System.out.println("Server started at http://localhost:8081");
     }
-
     static class HelloHandler implements HttpHandler {
         public void handle(HttpExchange exchange) throws IOException {
-            String response = "Hello Công Nghệ Phần Mềm";
+            String response = "Hello World from Jenkins + GitHub!";
             exchange.sendResponseHeaders(200, response.length());
             try (OutputStream os = exchange.getResponseBody()) {
                 os.write(response.getBytes());
